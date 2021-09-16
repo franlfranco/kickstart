16aef49ea51a178bd890fc9e22c69625d8a707fc
francofrancisco <franco.francisco@belonghome.com>
2021-09-16 10:15:39 -0300

    first commit

---
+++ ethereum/deploy.js
@@ -0,0 +1,24 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider(
+  'scrap evidence hunt police advance shell scrub empower struggle topic early skirt',
+  'https://rinkeby.infura.io/v3/6f1827cc208b4f57a32b3970374d99b5'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: '1000000', gasPrice: '5000000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
